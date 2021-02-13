05a20d8104eae95ec304766528839542784b98a3
Anup Bagali <anup.bagali1@gmail.com>
2021-02-13 17:53:02 -0500

    moar optimize

--- lab04/p1/l041.cpp
+++ lab04/p1/l041.cpp
@@ -155,7 +155,7 @@ bool reorganize(unordered_map<Point, vector<Point>, PointHash> &previous, Point 
             n++;
         }
         Point nm = Point(x/n, y/n);
-        if(nm != means[i])
+        if(organized && (nm != means[i]))
             organized = false;
         means[i] = nm;
     }
