fd3542e69c9263643208afe53c029c77734fbaba
Anup Bagali <2021abagali@borg6.csl.tjhsst.edu>
2021-03-22 06:39:40 +0000

    add precompute

--- lab05/p2/l052.cpp
+++ lab05/p2/l052.cpp
@@ -7,11 +7,14 @@
 #define uchar unsigned char
 
 #define INFILE "image.ppm"
-#define OUT1 "image1.ppm"
-#define OUT2 "image2.ppm"
-#define OUTFILE "imagef.ppm"
+#define NOUT "image1.ppm"
+#define HOUT "image2.ppm"
+#define FOUT "imagef.ppm"
 
-#define T 300
+#define RD 57.2957795131
+
+#define T1 70
+#define T2 120
 
 using namespace std;
 
@@ -20,12 +23,28 @@ int X, Y, N;
 const int SX[9] = {1, 0, -1, 2, 0, -2, 1, 0, -1};
 const int SY[9] = {1, 2, 1, 0, 0, 0, -1, -2 ,-1};
 
+const int ANGLES[4] = {0, 45, 135, 180};
+
 static inline bool edge(int i){
     return (i < X) || (!(i%X)) || !((i+1)%X) || (i > X*(Y-1));
 }
 
 static inline int dot(const int p[9], const int op[9]){
     return p[0]*op[0] + p[1]*op[1] + p[2]*op[2] + p[3]*op[3] + p[4]*op[4] + p[5]*op[5] + p[6]*op[6] + p[7]*op[7] + p[8]*op[8];
+}
+
+int closest_angle(const int &theta){
+    int t = abs(theta);
+    int min_dist = 181;
+    int angle, d;
+    for(const int &i: ANGLES){
+        d = abs(t-i);
+        if(d < min_dist){
+            min_dist = d;
+            angle = i;
+        }
+    }
+    return ((theta > 0) - (theta < 0)) * angle;
 }
 
 void write_ppm(const string &filename, vector<int> &img, int size = 1){
@@ -62,13 +81,14 @@ vector<int>* read_file(const string &filename){
     return grayscale;
 }
 
-void single_threshole(vector<int> &grayscale){
+void gradient(vector<int> &grayscale){
     vector<int> *gradient = new vector<int>(N, 0);
+    vector<int> *angles = new vector<int>(N, 0);
+    vector<int> *hyst = new vector<int>(N);
     int gx, gy;
+    double mag;
     for(int i=X+1;i<N-X;i++){
-        if(edge(i)){
-            gradient->at(i) = 0;
-        }else{
+        if(!edge(i)){
             int grad[9] = {
                 grayscale[i-X-1], grayscale[i-X], grayscale[i-X+1],
                 grayscale[i-1]  , grayscale[i]  , grayscale[i+1]  ,
@@ -76,16 +96,20 @@ void single_threshole(vector<int> &grayscale){
             };
             gx = dot(grad, SX);
             gy = dot(grad, SY);
-            gradient->at(i) = (int)(sqrt(pow(gx, 2) + pow(gy, 2)) > T);
+            mag = sqrt(pow(gx, 2) + pow(gy, 2));
+            gradient->at(i) = (int)mag;
+            angles->at(i) = closest_angle(atan2(gy, gx) * RD);
+            hyst->at(i) = mag > T2 ? 2 : (mag > T1 ? 1 : 0);
         }
     }
-    write_ppm("test.ppm", *gradient);
     delete gradient;
+    delete angles;
+    delete hyst;
 }
 
 void part2(){
     vector<int> *grayscale = read_file("image.ppm");
-    single_threshole(*grayscale);
+    gradient(*grayscale);
     delete grayscale;
 }
 
