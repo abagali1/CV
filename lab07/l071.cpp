51675b49f37c2aa2128e1e08c0842ecf852f57ae
Anup Bagali <anup.bagali1@gmail.com>
2021-04-30 18:02:17 -0400

    what the heck is a hough transform

--- lab07/l071.cpp
+++ lab07/l071.cpp
@@ -16,14 +16,40 @@ using namespace cv;
 
 int main(int argc, char** argv )
 {
-    Mat orig, grayscale, edges;
-    orig = imread(INFILE, IMREAD_COLOR);
-    cvtColor(orig, grayscale, COLOR_BGR2GRAY);
+    Mat src, grayscale, edges, cdst;
+    src = imread(INFILE, IMREAD_COLOR);
+    cvtColor(src, grayscale, COLOR_BGR2GRAY);
+    int X = src.cols, Y = src.rows;
     
     blur(grayscale, edges, Size(3,3));
     Canny(edges, edges, LT, HT, 3);
-    
-    imwrite("./gray.jpg", grayscale);
-    imwrite("./edges.jpg", edges);
+
+    cvtColor(edges, cdst, COLOR_GRAY2BGR);
+
+    vector<Vec2f> lines;
+    HoughLines(edges, lines, 1, CV_PI/180, 50, 0 ,0);
+
+    float rho, theta;
+    Point p1, p2;
+    double a, b, x0, y0;
+    for(size_t i=0;i<lines.size();i++){
+        rho = lines[i][0];
+        theta = lines[i][1];
+        a = cos(theta);
+        b = sin(theta);
+        x0 = a*rho;
+        y0 = a*rho;
+        p1.x = cvRound(x0 + X*(-b));
+        p1.y = cvRound(y0 + Y*(a));
+        p2.x = cvRound(x0 - X*(-b));
+        p2.y = cvRound(y0 - Y*(a));
+        line(cdst, p1, p2, Scalar(0, 0, 255), 3, LINE_AA);
+    }
+
+    imwrite("./gray.png", grayscale);
+    imwrite("./edges.png", edges);
+    imwrite("./lines.png", cdst);
+
+    waitKey();
     
 }
